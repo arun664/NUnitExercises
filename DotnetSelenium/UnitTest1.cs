906d3427893fc46a923c6450b259af06b133bebd
ArunKumarM <arunmnayaka@gmail.com>
2025-03-16 10:08:57 -0400

    Basic locators usage test

--- DotnetSelenium/UnitTest1.cs
+++ DotnetSelenium/UnitTest1.cs
@@ -22,5 +22,18 @@ namespace DotnetSelenium
             googleSearchInputBox.SendKeys("dotnet selenium");
             googleSearchInputBox.SendKeys(Keys.Return);
         }
+
+        [Test]
+        public void EaWebsiteLogin()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(@"http://eaapp.somee.com/");
+
+            driver.FindElement(By.Id("loginLink")).Click();
+            driver.FindElement(By.Name("UserName")).SendKeys("admin");
+            driver.FindElement(By.Name("Password")).SendKeys("password");
+            driver.FindElement(By.CssSelector(".btn")).Submit();
+        }
     }
 }
